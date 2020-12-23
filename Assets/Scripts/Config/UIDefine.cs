46123cb424421c6a84d20236a4cc512e5eb216c1
dreamCirno <240773979@qq.com>
2020-12-23 14:31:17 +0800

    定义面板类型、显示方式、透明度的枚举类型

---
+++ Assets/Scripts/Config/UIDefine.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+
+namespace Cirnoclassroom.UI {
+    /// <summary>
+    /// 用于框架定义常量、全局性方法、枚举类型、委托定义
+    /// </summary>
+    public class UIDefine : MonoBehaviour {
+
+        #region Variables
+
+        #region Enum
+
+        /// <summary>
+        /// 面板类型
+        /// </summary>
+        public enum PanelType {
+            /// <summary>
+            /// 普通
+            /// </summary>
+            Normal,
+            /// <summary>
+            /// 固定
+            /// </summary>
+            Fixed,
+            /// <summary>
+            /// 弹窗
+            /// </summary>
+            Popup,
+        }
+
+        /// <summary>
+        /// 面板显示方式
+        /// </summary>
+        public enum PanelDisplayMode {
+            /// <summary>
+            /// 正常显示面板
+            /// </summary>
+            Normal,
+            /// <summary>
+            /// 顺序显示面板
+            /// </summary>
+            Sequence,
+            /// <summary>
+            /// 仅显示当前面板
+            /// </summary>
+            Alone,
+        }
+
+        /// <summary>
+        /// 面板透明度类型
+        /// </summary>
+        public enum PanelAlphaType {
+            /// <summary>
+            /// 完全透明，不可穿透
+            /// </summary>
+            Transparent,
+            /// <summary>
+            /// 半透明，不可穿透
+            /// </summary>
+            Translucent,
+            /// <summary>
+            /// 低透明，不可穿透
+            /// </summary>
+            LowTransparent,
+            /// <summary>
+            /// 可穿透
+            /// </summary>
+            Penetrable,
+        }
+
+        #endregion
+
+        #endregion
+
+        #region MainMethods
+
+        void Start() {
+
+        }
+
+        void Update() {
+
+        }
+
+        #endregion
+
+        #region HelperMethods
+
+
+
+        #endregion
+
+    }
+}
